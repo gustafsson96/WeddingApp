774939b440386cb119875002a9c3e71a0f2606e0
Julia Gustafsson <juliagustafsson96@hotmail.com>
2026-02-19 14:27:40 +0100

    Modify Wedding controller to link a wedding to the logged in user and give them full ownership of CRUD

--- Controllers/WeddingsController.cs
+++ Controllers/WeddingsController.cs
@@ -2,27 +2,44 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using WeddingApp.Data;
 using WeddingApp.Models;
 
+[Authorize]
+// Scaffolded and modified Wedding controller. Requires authorization.
 namespace WeddingApp.Controllers
 {
     public class WeddingsController : Controller
     {
+        // Used to get the logged in user
+        private readonly UserManager<IdentityUser> _userManager;
+
+        // DbContext to access database
         private readonly ApplicationDbContext _context;
 
-        public WeddingsController(ApplicationDbContext context)
+        public WeddingsController(
+            ApplicationDbContext context,
+            UserManager<IdentityUser> userManager
+        )
         {
             _context = context;
+            _userManager = userManager;
         }
 
         // GET: Weddings
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Weddings.ToListAsync());
+            // Get logged in user
+            var user = await _userManager.GetUserAsync(User);
+
+            // Get wedding that belongs to logged in user
+            var weddings = await _context.Weddings.Where(w => w.UserId == user.Id).ToListAsync();
+
+            return View(weddings);
         }
 
         // GET: Weddings/Details/5
@@ -33,11 +50,21 @@ namespace WeddingApp.Controllers
                 return NotFound();
             }
 
-            var wedding = await _context.Weddings
-                .FirstOrDefaultAsync(m => m.WeddingId == id);
+            // Get wedding from database
+            var wedding = await _context.Weddings.FirstOrDefaultAsync(m => m.WeddingId == id);
+
             if (wedding == null)
             {
                 return NotFound();
+            }
+
+            // Get logged in user
+            var user = await _userManager.GetUserAsync(User);
+
+            // Safety check - only the owner can view their wedding
+            if (wedding.UserId != user.Id)
+            {
+                return Forbid();
             }
 
             return View(wedding);
@@ -54,12 +81,24 @@ namespace WeddingApp.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("WeddingId,UserId,FirstPerson,SecondPerson,Date,Time,Venue,City,AdditionalInfo,HeaderImageUrl")] Wedding wedding)
+        public async Task<IActionResult> Create(
+            [Bind("FirstPerson,SecondPerson,Date,Time,Venue,City,AdditionalInfo,HeaderImageUrl")]
+                Wedding wedding
+        )
         {
             if (ModelState.IsValid)
             {
+                // Get logged in user
+                var user = await _userManager.GetUserAsync(User);
+
+                // Link wedding to user
+                wedding.UserId = user.Id;
+
+                // Add to database
                 _context.Add(wedding);
                 await _context.SaveChangesAsync();
+
+                // Redirect to dashboard after POST
                 return RedirectToAction(nameof(Index));
             }
             return View(wedding);
@@ -68,7 +107,41 @@ namespace WeddingApp.Controllers
         // GET: Weddings/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null)
+            // Get wedding
+            var wedding = await _context.Weddings.FindAsync(id);
+
+            if (wedding == null)
+            {
+                return NotFound();
+            }
+
+            // Get logged in user
+            var user = await _userManager.GetUserAsync(User);
+
+            // Safety check - only owner can edit
+            if (wedding.UserId != user.Id)
+            {
+                return Forbid();
+            }
+
+            return View(wedding);
+        }
+
+        // POST: Weddings/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(
+            int id,
+            [Bind(
+                "WeddingId,FirstPerson,SecondPerson,Date,Time,Venue,City,AdditionalInfo,HeaderImageUrl"
+            )]
+                Wedding updatedWedding
+        )
+        {
+            // Check that id match for original and updated wedding
+            if (id != updatedWedding.WeddingId)
             {
                 return NotFound();
             }
@@ -78,42 +151,33 @@ namespace WeddingApp.Controllers
             {
                 return NotFound();
             }
-            return View(wedding);
-        }
 
-        // POST: Weddings/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("WeddingId,UserId,FirstPerson,SecondPerson,Date,Time,Venue,City,AdditionalInfo,HeaderImageUrl")] Wedding wedding)
-        {
-            if (id != wedding.WeddingId)
+            // Get logged in user
+            var user = await _userManager.GetUserAsync(User);
+            // Safety check - only owner can save edit
+            if (wedding.UserId != user.Id)
             {
-                return NotFound();
+                return Forbid();
             }
 
             if (ModelState.IsValid)
             {
-                try
-                {
-                    _context.Update(wedding);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!WeddingExists(wedding.WeddingId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
+                // Only update allowed fields
+                wedding.FirstPerson = updatedWedding.FirstPerson;
+                wedding.SecondPerson = updatedWedding.SecondPerson;
+                wedding.Date = updatedWedding.Date;
+                wedding.Time = updatedWedding.Time;
+                wedding.Venue = updatedWedding.Venue;
+                wedding.City = updatedWedding.City;
+                wedding.AdditionalInfo = updatedWedding.AdditionalInfo;
+                wedding.HeaderImageUrl = updatedWedding.HeaderImageUrl;
+
+                await _context.SaveChangesAsync();
+
                 return RedirectToAction(nameof(Index));
             }
-            return View(wedding);
+
+            return View(updatedWedding);
         }
 
         // GET: Weddings/Delete/5
@@ -124,11 +188,19 @@ namespace WeddingApp.Controllers
                 return NotFound();
             }
 
-            var wedding = await _context.Weddings
-                .FirstOrDefaultAsync(m => m.WeddingId == id);
+            var wedding = await _context.Weddings.FirstOrDefaultAsync(m => m.WeddingId == id);
             if (wedding == null)
             {
                 return NotFound();
+            }
+
+            // Get logged in user
+            var user = await _userManager.GetUserAsync(User);
+
+            // Safety check - only owner can delete
+            if (wedding.UserId != user.Id)
+            {
+                return Forbid();
             }
 
             return View(wedding);
@@ -140,18 +212,24 @@ namespace WeddingApp.Controllers
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var wedding = await _context.Weddings.FindAsync(id);
-            if (wedding != null)
+            if (wedding == null)
             {
-                _context.Weddings.Remove(wedding);
+                return NotFound();
+            }
+            // Get logged in user
+            var user = await _userManager.GetUserAsync(User);
+
+            // Safety check - only owner can perform a delete
+            if (wedding.UserId != user.Id)
+            {
+                return Forbid();
             }
 
+            _context.Weddings.Remove(wedding);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
 
-        private bool WeddingExists(int id)
-        {
-            return _context.Weddings.Any(e => e.WeddingId == id);
+            // Redirect to dashboard after delete
+            return RedirectToAction(nameof(Index));
         }
     }
 }
