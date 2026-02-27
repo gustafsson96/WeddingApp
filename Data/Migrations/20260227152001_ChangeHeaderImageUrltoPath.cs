0424718ec1b98514d3f0158f1bddbb870ea51c7b
Julia Gustafsson <juliagustafsson96@hotmail.com>
2026-02-27 16:30:33 +0100

    Migrate database after changing HeaderImageUrl to HeaderImagePath in Wedding model

---
+++ Data/Migrations/20260227152001_ChangeHeaderImageUrltoPath.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeddingApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeHeaderImageUrltoPath : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "HeaderImageUrl",
+                table: "Weddings",
+                newName: "HeaderImagePath");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "HeaderImagePath",
+                table: "Weddings",
+                newName: "HeaderImageUrl");
+        }
+    }
+}
